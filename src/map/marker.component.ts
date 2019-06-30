76d0c161f1be47ac9a1c56ac9e8f11df97e4a5e1
Branton Horsley <brantonhorsley@notnarb.com>
2019-06-30 00:16:03 -0700

    Added click EventEmitter to Marker elements

--- src/map/marker.component.ts
+++ src/map/marker.component.ts
@@ -1,8 +1,8 @@
-import {Component, ChangeDetectionStrategy, Input, OnInit, OnDestroy} from '@angular/core';
+import {Component, ChangeDetectionStrategy, Input, OnInit, OnDestroy, Output, EventEmitter} from '@angular/core';
 import { MapApiService } from './map-api.service';
 import {BehaviorSubject, Subject, ReplaySubject, combineLatest} from 'rxjs';
 import {takeUntil } from 'rxjs/operators';
-import {GoogleMap, GoogleMapsMarker, MarkerOptions} from './map-types';
+import {GoogleMap, GoogleMapsMarker, MarkerOptions, MapsEventListener} from './map-types';
 
 /** Google Maps Marker component */
 @Component({
@@ -14,11 +14,13 @@ export class MarkerComponent implements OnInit, OnDestroy {
   @Input() set options(options: MarkerOptions) {
     this.options$.next(options);
   }
+  @Output() click = new EventEmitter<void>();
 
   private readonly destroy$ = new Subject<void>();
   private readonly options$ = new BehaviorSubject<MarkerOptions>({});
   private readonly map$ = new ReplaySubject<GoogleMap>(1);
   private marker?: GoogleMapsMarker;
+  private clickListener?: MapsEventListener;
 
   constructor(private readonly mapApiService: MapApiService) {}
 
@@ -29,6 +31,10 @@ export class MarkerComponent implements OnInit, OnDestroy {
       if (!this.marker) {
         this.marker = new mapsApi.Marker(options);
         this.marker.setMap(map);
+
+        // TODO(bhorsley): Should these be conditionally added to reduce the
+        // number of active listeners?
+        this.clickListener = this.marker.addListener('click', () => this.click.emit());
       } else {
         this.marker.setOptions(options);
       }
@@ -38,8 +44,13 @@ export class MarkerComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+
     if (this.marker) {
       this.marker.setMap(null);
+    }
+
+    if (this.clickListener) {
+      this.clickListener.remove();
     }
   }
 
